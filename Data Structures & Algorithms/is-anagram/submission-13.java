c3d611d22f35580c3ffde72e6c8f94f139595d2c
Josh Jiang <68052349+Tianyao-Jiang@users.noreply.github.com>
2026-08-01 18:43:27 +1000

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] counter = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a'] += 1;
+            counter[t.charAt(i) - 'a'] -= 1;
+        }
+
+        for (Integer i : counter) {
+            if (i != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
