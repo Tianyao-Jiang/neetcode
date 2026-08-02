5c56b988e257e08584442be848104569b7a428f0
Josh Jiang <68052349+Tianyao-Jiang@users.noreply.github.com>
2026-08-02 10:42:15 +1000

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+        while (l < r) {
+            while(l < r && !isAlphaNum(s.charAt(l))) {
+                l++;
+            }
+            while(l < r && !isAlphaNum(s.charAt(r))) {
+                r--;
+            }
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    private boolean isAlphaNum(char c) {
+        return (c <= 'z' && c >= 'a') ||
+               (c <= 'Z' && c >= 'A') || 
+               (c <= '9' && c >= '0');
+    }
+}
