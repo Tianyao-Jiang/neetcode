9c50b0b1a640d7f41193302c47c288d3de8b9e68
Josh Jiang <68052349+Tianyao-Jiang@users.noreply.github.com>
2026-08-31 14:52:52 +1000

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> curRes = new ArrayList<>();
+        dfs(res, curRes, 0, nums);
+
+        return res;
+    }
+
+    private void dfs(List<List<Integer>> res, List<Integer> curRes, int cur, int[] nums) {
+
+        // for each node, check if it is leave
+        if (cur >= nums.length) {
+            res.add(new ArrayList<>(curRes));
+            return;
+        }
+
+        // left put
+        curRes.add(nums[cur]);
+        dfs(res, curRes, cur + 1, nums);
+
+        // right no put
+        curRes.remove(curRes.size() - 1);
+        dfs(res, curRes, cur + 1, nums);
+
+        return;
+    }
+}
