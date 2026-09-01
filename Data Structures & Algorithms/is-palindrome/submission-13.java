61c29df82742eb09bde00cb5f437ff0235668bc1
Josh Jiang <68052349+Tianyao-Jiang@users.noreply.github.com>
2026-09-01 21:46:01 +1000

    Add: is-palindrome - submission-13

---
+++ Data Structures & Algorithms/is-palindrome/submission-13.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l <= r) {
+            if (!isValid(s.charAt(l))) {
+                l++;
+                continue;
+            }
+
+            if (!isValid(s.charAt(r))) {
+                r--;
+                continue;
+            }
+
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false;
+            }
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    private boolean isValid(char c) {
+        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
+            return true;
+        }
+
+        return false;
+    }
+}
